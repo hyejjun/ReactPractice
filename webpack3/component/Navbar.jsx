5d6e3d4aa60cddbbeccd0f52a1fd359039152103
hyejjun <hjlee9725@gmail.com>
2021-07-06 15:04:44 +0900

    0706 webpack2,3

---
+++ webpack3/component/Navbar.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import '../css/Navbar.css'
+import { FaFacebook, FaInstagram, FaBeer, FaTwitter, FaBars } from 'react-icons/fa'
+
+class Navbar extends Component {
+    state = {
+        social: [
+            {
+                id: 1,
+                url: 'https://www.twitter.com',
+                icons: <FaTwitter />
+            },
+            {
+                id: 2,
+                url: 'https://www.facebook.com',
+                icons: <FaFacebook />
+            },
+            {
+                id: 3,
+                url: 'https://www.instagram.com',
+                icons: <FaInstagram />
+            },
+        ],
+
+        menu: [
+            {
+                id: 1,
+                url: '/',
+                text: 'home'
+            },
+            {
+                id: 2,
+                url: '/',
+                text: 'about'
+            },
+            {
+                id: 3,
+                url: '/',
+                text: 'project'
+            },
+        ],
+
+        flag: false,
+
+    }
+
+    MobileNavi = (e) => {
+        const menubar = document.querySelector('#links-container');
+        let { flag } = this.state
+
+        switch (flag) {
+            case true:
+                menubar.setAttribute('class','links-container')
+                this.setState({ flag: false })
+                break;
+
+            case false:
+                menubar.setAttribute('class','open')
+                this.setState({ flag: true })
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    render() {
+        return (
+            <nav>
+                <div className="nav-center">
+                    {/* Logo */}
+                    <div className="nav-header">
+                        <h1 className="logo">Logo</h1>
+                        <button className="nav-toggle" onClick={this.MobileNavi}>
+                            <FaBars />
+                        </button>
+                    </div>
+
+                    {/* Navigation */}
+                    <div id="links-container" className="links-container">
+                        <ul className="links">
+                            {
+                                this.state.menu.map(item => {
+                                    const { id, url, text } = item
+                                    return (
+                                        <li key={id}>
+                                            <a href={url}>{text}</a>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <ul className="social-icons">
+                        {
+                            this.state.social.map(item => {
+                                const { id, url, icons } = item    //비구조 할당문
+                                return (
+                                    <li key={id}>
+                                        <a href={url}>{icons}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
